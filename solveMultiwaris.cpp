3b017530a31a5322af3d8a8eb3ec7dd9ec364c2e
aldys002 <igidiaaldys@gmail.com>
2024-05-28 14:47:58 +0700

    menambah tanda titik koma pada akhir kurung kurawal

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -14,4 +14,17 @@ public :
 
 class pekerja : public orang {
 public :
-}
+
+    pekerja(int pUmur) :
+          orang(pUmur)
+    {
+        cout << "Pekerja dibuat\n" << endl;
+    }
+};
+
+class pelajar : public orang {
+public :
+
+
+
+};
