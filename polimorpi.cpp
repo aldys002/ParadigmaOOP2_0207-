99310c075f5ae4b7890b4b936d3ba36b5b6498f6
aldys002 <igidiaaldys@gmail.com>
2024-05-28 15:22:19 +0700

    menambah orang pada class budi

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -4,9 +4,9 @@ using namespace std;
 class seseorang {
 public:
     virtual void pesan() = 0;
-    virtual void pesan(){
-        cout << "Pesan dari seseorang" << endl;
-    }
+    //virtual void pesan(){
+    //    cout << "Pesan dari seseorang" << endl;
+    //}
 };
 
 class joko :public seseorang {
@@ -24,5 +24,15 @@ public:
 };
 
 int main() {
-    
+    seseorang* obyek;
+    joko a;
+    lia b;
+
+    obyek = &a;
+    obyek->pesan();
+    obyek = &b;
+    obyek->pesan();
+    //a. seseorang::pesan();
+    return;
+
 }
