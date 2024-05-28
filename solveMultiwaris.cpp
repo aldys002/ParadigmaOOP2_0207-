98ee25b4f96855753056acaf3d3d3e6da8550b6b
aldys002 <igidiaaldys@gmail.com>
2024-05-28 14:42:40 +0700

    membuat class pekerja

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 
 class orang {
+public :
+   int umur;
 
+   orang(int pUmur) :
+       umur(pUmur)
+       {
+        cout << "orang dibuat dengan umur" << umur <<"\n" << endl;
+       }
 };
+
+class pekerja : public orang {
+public :
+}
